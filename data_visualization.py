68176b72ce31534c610700228f681285672499d7
keemjoonsung <kjs990114@naver.com>
2024-11-25 15:28:34 +0900

    update data-visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -2,16 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load dataset
 file_path = './large_dataset.csv'
 large_dataset = pd.read_csv(file_path)
 
-# r_fighter기준으로 승/패 변환
+# r_fighter 기준으로 승/패 변환
 large_dataset['result'] = large_dataset['winner'].apply(lambda x: 'win' if x == 'Red' else 'lose')
 
-selected_columns = ['kd_diff', 'age_diff', 'height_diff', 'weight_diff', 'reach_diff']
+diff_columns = [col for col in large_dataset.columns if '_diff' in col]
 
-# box plot
-for col in selected_columns:
+#  박스플롯 시각화
+for col in diff_columns:
     plt.figure(figsize=(10, 6))
     sns.boxplot(data=large_dataset, x='result', y=col, palette='cool')
     plt.title(f"Distribution of {col} by r_fighter Win/Lose")
@@ -20,8 +21,8 @@ for col in selected_columns:
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
 
-# line graph
-for col in selected_columns:
+# 선 그래프 시각화
+for col in diff_columns:
     # diff 값을 구간별로 나누고 각 구간에서의 승률 계산
     bins = pd.qcut(large_dataset[col], q=10, duplicates='drop') 
     win_rate_by_bin = large_dataset.groupby(bins)['result'].apply(lambda x: (x == 'win').mean())
